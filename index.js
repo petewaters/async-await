d9c110a08a6d4d96e77d7a71196a7670297cfb4f
Pete Waters <pete.d.waters@gmail.com>
2017-08-14 23:41:29 +0100

    Implemented async version of fetchAvatarUrl

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const fetch = require('node-fetch')
 
-function fetchAvatarUrl(userId) {
-    return fetch(`https://catappapi.herokuapp.com/users/${userId}`)
-        .then(response => response.json())
-        .then(data => data.imageUrl)
+async function fetchAvatarUrl(userId) {
+    const response = await fetch(`https://catappapi.herokuapp.com/users/${userId}`)
+    const data = await response.json()
+    response
+    data
+    return data.imageUrl
+
+
+    // return fetch(`https://catappapi.herokuapp.com/users/${userId}`)
+    //     .then(response => response.json())
+    //     .then(data => data.imageUrl)
 }
 
 const result = fetchAvatarUrl(123)
