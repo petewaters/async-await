91ae33e204234e221205bb0cfa06616e20f153d1
Pete Waters <pete.d.waters@gmail.com>
2017-08-15 00:12:47 +0100

    Added fetchCatAvatars synchronous function

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const fetch = require('node-fetch')
 
-async function fetchAvatarUrl(userId) {
-    const response = await fetch(`https://catappapi.herokuapp.com/users/${userId}`)
-    const data = await response.json()
-    response
-    data
-    return data.imageUrl
-
-
-    // return fetch(`https://catappapi.herokuapp.com/users/${userId}`)
-    //     .then(response => response.json())
-    //     .then(data => data.imageUrl)
+function fetchAvatarUrl(userId) {
+    return fetch(`https://catappapi.herokuapp.com/users/${userId}`)
+        .then(response => response.json())
+        .then(data => data.imageUrl)
 }
 
 const result = fetchAvatarUrl(123)
 result 
+
+async function asyncfetchAvatarUrl(userId) {
+    const response = await fetch(`https://catappapi.herokuapp.com/users/${userId}`)
+    const data = await response.json()
+    return data.imageUrl
+}
+
+const asyncResult = asyncfetchAvatarUrl(123)
+asyncResult 
+
+function fetchCatAvatars(userId) {
+    return fetch(`https://catappapi.herokuapp.com/users/${userId}`)
+        .then(response => response.json())
+        .then(user => {
+            const promises = user.cats.map(catId => 
+                fetch(`https://catappapi.herokuapp.com/cats/${catId}`)
+                    .then(response => response.json())
+                    .then(catData => catData.imageUrl)
+            )
+            return Promise.all(promises)
+        })
+}
+
+const avatarResult = fetchCatAvatars(123)
+avatarResult
 
